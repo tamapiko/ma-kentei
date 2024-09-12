7cff250546667c6e63f5cc56b70caabc55b7fa12
タマピコ <125553567+tamapiko@users.noreply.github.com>
2024-09-12 19:19:48 +0900

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('file-upload').onchange = function () {
-    let fileName = this.files.length > 1 ? this.files.length + ' files selected' : this.files[0].name;
-    this.nextElementSibling.innerText = fileName;
-};
-
-document.getElementById('answer-upload').onchange = function () {
-    let fileName = this.files[0].name;
-    this.nextElementSibling.innerText = fileName;
-};
